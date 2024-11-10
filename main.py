148933ac4c41f5ee0316fd9ad8d15f382fbb24ae
ebongat <ebongat1762@gmail.com>
2024-11-10 13:42:19 -0800

    added circleshape.py and player.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    color = (0, 0, 0)
+    color = "black"
     running = True
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color)
+        player.draw(screen)
         pygame.display.flip()
+        
+        #limits framerate to 60
+        dt = clock.tick(60) / 1000
+
 
 
 
